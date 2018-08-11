3af0858b5b93ddeaefbe5cc0182a96a3a0d5557a
Damian <32736054+DamianZalewski@users.noreply.github.com>
2018-08-11 12:27:06 +0200

    initial files commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 600;
+var cw = canvas.width;
+var ch = canvas.height;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0,0,cw,ch);
