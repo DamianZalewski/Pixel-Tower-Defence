cdd3b95db61cfaffd4d53c7a106279410d8f53d5
Damian Zalewski <damian_zalewskipl@o2.pl>
2018-09-21 20:56:31 +0200

    Fixed tower bug plus added map round functionality

--- scripts/ui.js
+++ scripts/ui.js
@@ -32,7 +32,7 @@ mainMenuImage.src = "assets/mainMenu.png";
 
 
 function drawSideMenu() {
-    towerChoice == 1 ? ctx.fillStyle = "red" : ctx.fillStyle = "white";
+    towerChoice == 'archerTower' ? ctx.fillStyle = "red" : ctx.fillStyle = "white";
     ctx.fillRect(cw-200,100,54,54);
     ctx.drawImage(archerTower,cw-200,100,54,54);
     ctx.fillStyle = "black";
@@ -42,7 +42,7 @@ function drawSideMenu() {
     ctx.font = "16px Arial";
     ctx.fillText('15',cw-175,166);
     
-    towerChoice == 2 ? ctx.fillStyle = "red" : ctx.fillStyle = "white";
+    towerChoice == 'mageTower' ? ctx.fillStyle = "red" : ctx.fillStyle = "white";
     ctx.fillRect(cw-100,100,54,54);
     ctx.drawImage(mageTower,cw-100,100,54,54);
     ctx.fillStyle = "black";
@@ -55,6 +55,13 @@ function drawSideMenu() {
     drawGold();
     drawLife();
     drawEnemyInfo();
+    drawStage();
+}
+
+function drawStage() {
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    ctx.fillText('stage '+(levelRounds+1)+'/2',cw-120, 60);
 }
 
 function handleSideMenuClick(ev) {
@@ -64,13 +71,13 @@ function handleSideMenuClick(ev) {
        posX >= cw-200 && posX <= cw - 146 &&
        posY >= 100 && posY <= 154
    ) {
-       towerChoice = 1;
+       towerChoice = 'archerTower';
    }
    else    if(
        posX >= cw-100 && posX <= cw - 46 &&
        posY >= 100 && posY <= 154
    ) {
-       towerChoice = 2;
+       towerChoice = 'mageTower';
    }
     
 }
