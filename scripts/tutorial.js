94816fc1dbc5177706be3a53226e8246669b3c9b
Damian Zalewski <damian_zalewskipl@o2.pl>
2018-10-24 18:16:20 +0200

    updated tutorial scene

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -5,6 +5,12 @@ function drawTutorial() {
         case 0:
             drawTutorialStage0();
             break;
+        case 1:
+            drawTutorialStage1();
+            break;
+        case 2:
+            drawTutorialStage2();
+            break;
     }
 }
 
@@ -16,10 +22,16 @@ function handleTutorialClick(ev) {
         case 0:
             handleTutorial0Click(posX,posY);
             break;
+        case 1:
+            handleTutorial1Click(posX,posY);
+            break;
+        case 2:
+            handleTutorial2Click(posX,posY);
+            break;
     }
 }
 
-
+// tutorial 0 ---
 function drawTutorialStage0() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,cw,ch);
@@ -40,11 +52,72 @@ function handleTutorial0Click(posX, posY) {
        posX >= cw/2-150 && posX <= cw/2-50 &&
        posY >= 262 && posY <= 312
     ) {
-        console.log('yes');
+        tutorialStage = 1;
     } else if(
        posX >= cw/2+50 && posX <= cw/2+150 &&
        posY >= 262 && posY <= 312
     ) {
         setGameStage('adventureMapMenu');
+    }
+}
+
+
+// tutorial 1 ---
+function drawTutorialStage1() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,cw,ch);
+    
+    // tutorial character
+    ctx.fillStyle = "blue";
+    ctx.fillRect(cw-300,150,300,ch-150);
+    
+    // dialog
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(0,ch-150,cw,150);
+    
+    // next button
+    ctx.fillStyle = 'red';
+    ctx.fillRect(cw-50,ch-50,40,40);
+    
+    // dialog text
+    ctx.fillStyle = "black";
+    ctx.textAlign = 'left';
+    ctx.font = '36px Arial';
+    ctx.fillText('Welcome to PIXEL TOWER DEFENCE tutorial!',50,ch-80,cw-100);
+}
+
+function handleTutorial1Click(posX, posY) {
+    if(
+       posX >= cw-50 && posX <= cw-10 &&
+       posY >= ch-50 && posY <= ch-10
+    ) {
+        tutorialStage = 2;
+    }
+}
+
+// tutorial 2 ---
+
+function drawTutorialStage2() {
+    // dialog
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(0,ch-150,cw,150);
+    
+    // next button
+    ctx.fillStyle = 'red';
+    ctx.fillRect(cw-50,ch-50,40,40);
+    
+    // dialog text
+    ctx.fillStyle = "black";
+    ctx.textAlign = 'left';
+    ctx.font = '36px Arial';
+    ctx.fillText('Your goal is to defence your city from the enemies!',50,ch-80,cw-100);
+}
+
+function handleTutorial2Click(posX, posY) {
+    if(
+       posX >= cw-50 && posX <= cw-10 &&
+       posY >= ch-50 && posY <= ch-10
+    ) {
+        tutorialStage = 3;
     }
 }
